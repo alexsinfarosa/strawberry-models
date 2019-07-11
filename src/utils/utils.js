0970896c2074723ce01ef11ae284b714f2600bae
alexsinfarosa <as898@cornell.edu>
2019-07-11 12:14:19 +0200

    Update: add new station network (nwon)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -26,6 +26,7 @@ export const matchIconsToStations = (station, state) => {
     network === "miwx" ||
     network === "oardc" ||
     network === "nysm" ||
+    network === "nwon" ||
     ((network === "cu_log" || network === "culog") && station.state !== "NY")
   ) {
     return station.state === postalCode || postalCode === "ALL"
@@ -47,6 +48,15 @@ export const matchIconsToStations = (station, state) => {
 };
 
 export const vXDef = {
+  nwon: {
+    pcpn: 5,
+    temp: 23,
+    rhum: 24,
+    lwet: 118,
+    wspd: 28,
+    wdir: 27,
+    srad: 132
+  },
   newa: {
     pcpn: 5,
     temp: 23,
